5eb82ebe0cf08bdee53ca02d9427e5fbde4dbfe6
congthangbg <congthangbg@gmail.com>
2021-05-30 15:59:33 +0700

    thangdcBE

---
+++ src/main/java/com/vn/SpringJava5AssShopApplication.java
@@ -0,0 +1,13 @@
+package com.vn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJava5AssShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJava5AssShopApplication.class, args);
+	}
+
+}

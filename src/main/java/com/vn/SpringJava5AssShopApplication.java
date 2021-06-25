de3d5d7740b4b1ba9951e1723a0aaf013ea3b261
congthangbg <congthangbg@gmail.com>
2021-06-25 07:09:55 +0700

    update cac file config

--- src/main/java/com/vn/SpringJava5AssShopApplication.java
+++ src/main/java/com/vn/SpringJava5AssShopApplication.java
@@ -1,13 +1,26 @@
 package com.vn;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.vn.config.StorageProperties;
+import com.vn.service.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SpringJava5AssShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJava5AssShopApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init (StorageService storageService) {
+		return (args -> {
+			storageService.init();
+		});
+	}
 }
